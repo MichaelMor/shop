c06e010be94fa8670b7f31289d763a201c502096
optimus450 <78796321+optimus450@users.noreply.github.com>
2022-01-06 17:02:36 +0200

    Prepare for deployment

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,12 +21,12 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>DAMBASHOP</Navbar.Brand>
+            <Navbar.Brand>ProShop</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Route render={({ history }) => <SearchBox history={history} />} />
-            <Nav className='ml-auto'>
+            <Nav className='ms-auto'>
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <i className='fas fa-shopping-cart'></i> Cart
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,13 @@ import store from './store'
 import './bootstrap.min.css'
 import './index.css'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
+
 
 ReactDOM.render(
+  //Provide the access to store and render//
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
 )
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
